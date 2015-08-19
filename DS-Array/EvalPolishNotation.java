48e444b12e38d464695492cd5b808475ffc4ccbb
deepti_deshpande <dxd132630@utdallas.edu>
2015-08-18 21:17:44 -0700

    Create EvalPolishNotation.java

---
+++ DS-Array/EvalPolishNotation.java
@@ -0,0 +1,31 @@
+//import java.util.stack ;
+import java.util.* ;
+
+public class EvalPolishNotation{
+	public static void main(String args[]){
+		String[] input = {"4", "13", "5", "/", "+"};
+		System.out.println(evalPN(input));
+	}
+	public static int evalPN(String[] input){
+		int returnVal = 0;
+		Stack<String> st = new Stack<>();
+		String operators = "+-*/";
+		for(String t: input){
+			if(!operators.contains(t))
+				st.push(t);
+			else{
+				int a = Integer.valueOf(st.pop());
+				int b = Integer.valueOf(st.pop());
+				switch(t){
+					case "+"  : st.push(String.valueOf(a+b)); break;				
+					case "-"  : st.push(String.valueOf(a-b)); break;			
+					case "*"  : st.push(String.valueOf(a*b)); break;	
+					case "/"  : st.push(String.valueOf(a/b)); break;					
+				}
+			}		
+		}
+			returnVal = Integer.valueOf(st.pop());
+			return returnVal;
+		
+	}
+}
