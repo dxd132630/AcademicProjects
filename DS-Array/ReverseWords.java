7ac73896f006a1e558de41e10f1378e01236b3f9
dxd132630 <dxd132630@utdallas.edu>
2015-08-24 16:46:19 -0500

    StrRev modified

--- DS-Array/ReverseWords.java
+++ DS-Array/ReverseWords.java
@@ -1,11 +1,27 @@
 public class ReverseWords{
+	public static String revStr(String input){
+		String out = "";
+		char[] temp = input.toCharArray();
+		for(int i=temp.length-1;i>=0;i--){
+			out += Character.toString(temp[i]); 
+		}
+		
+		return out;
+	}
     public static void main(String args[]){
-    String str[] = "string to be reversed".split(" ");
+    	String in = "string to be reversed";
+	    String str[] = in.split(" ");
+	    
+	    String finalStr="";
+	    System.out.println("in ="+in);
+	    finalStr = revStr(in);
+	    str = finalStr.split(" ");
+	    in = "";
+	    for(int i= 0;i<str.length;i++){
+	    	in+= revStr(str[i])+ " ";
+	    }
+	    System.out.println("rev ="+in);
+	    
+	}
     
-    String finalStr="";
-        for(int i = str.length-1; i>= 0 ;i--){
-            finalStr += str[i]+" ";
-        }
-        System.out.println(finalStr);
-    }
 }
